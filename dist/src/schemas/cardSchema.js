52d27758861d03970620a1fb2487e4af114f45da
AlineCantalice <alinedecantalice@gmail.com>
2022-09-12 15:17:38 -0300

    atualiza schema

--- dist/src/schemas/cardSchema.js
+++ dist/src/schemas/cardSchema.js
@@ -7,9 +7,9 @@ const joi_1 = __importDefault(require("joi"));
 const cardSchema = joi_1.default.object({
     cardName: joi_1.default.string().required(),
     cardNumber: joi_1.default.string().required(),
-    password: joi_1.default.string().required(),
+    password: joi_1.default.string().max(4).required(),
     securityCode: joi_1.default.string().max(3).required(),
-    expirationDate: joi_1.default.string().required(),
+    expirationDate: joi_1.default.string().max(5).required(),
     isVirtual: joi_1.default.boolean().required(),
     type: joi_1.default.valid('CREDIT', 'DEBIT', 'BOTH').required(),
     title: joi_1.default.string().required()
