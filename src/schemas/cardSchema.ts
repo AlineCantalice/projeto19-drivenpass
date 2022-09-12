f83ddfb13d8b5b8e34e036e562fb1cff1f7e3306
AlineCantalice <alinedecantalice@gmail.com>
2022-09-12 15:15:05 -0300

    atualiza dependencias

--- src/schemas/cardSchema.ts
+++ src/schemas/cardSchema.ts
@@ -3,9 +3,9 @@ import joi from "joi";
 const cardSchema = joi.object({
     cardName: joi.string().required(),
     cardNumber: joi.string().required(),
-    password: joi.string().required(),
+    password: joi.string().max(4).required(),
     securityCode: joi.string().max(3).required(),
-    expirationDate: joi.string().required(),
+    expirationDate: joi.string().max(5).required(),
     isVirtual: joi.boolean().required(),
     type: joi.valid('CREDIT', 'DEBIT', 'BOTH').required(),
     title: joi.string().required()
